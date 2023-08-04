520aec78a0aca3a7e7fc01b05cd52c665b71bb4d
Nika Oniani <nikaoniani2018@gmail.com>
2023-08-05 00:49:35 +0400

    worked a little on the navbar

--- src/templates/NavBar/NavBar.js
+++ src/templates/NavBar/NavBar.js
@@ -1,32 +1,75 @@
 import { NavLink, Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 import classes from "./NavBar.module.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 const NavBar = () => {
+  const location = useLocation();
+
+  const path = location.pathname;
+
   return (
     <>
       <Navbar className={classes.navBarBody} bg="dark" data-bs-theme="dark">
         <Container className={classes.navBarContainer}>
           <Navbar.Brand>
-            <NavLink to="/home">Books</NavLink>
+            <NavLink
+              className={() =>
+                path === "/home" ? classes.active : classes.link
+              }
+              to="/home"
+            >
+              Books
+            </NavLink>
           </Navbar.Brand>
           <Nav className="me-auto">
-            <NavLink to="/home/fantasy">
-              <li>Fantasy</li>
-            </NavLink>
-            <NavLink to="/home/adventure">
-              <li>Adventure</li>
-            </NavLink>
-            <NavLink to="/home/classics">
-              <li>Classics</li>
-            </NavLink>
-            <NavLink to="/home/science-fiction">
-              <li>Science Fiction</li>
-            </NavLink>
+            <Navbar.Brand>
+              <NavLink
+                className={(navData) =>
+                  navData.isActive ? classes.active : classes.link
+                }
+                to="/home/fantasy"
+              >
+                <li>Fantasy</li>
+              </NavLink>
+            </Navbar.Brand>
+
+            <Navbar.Brand>
+              <NavLink
+                className={(navData) =>
+                  navData.isActive ? classes.active : classes.link
+                }
+                to="/home/adventure"
+              >
+                <li>Adventure</li>
+              </NavLink>
+            </Navbar.Brand>
+
+            <Navbar.Brand>
+              <NavLink
+                className={(navData) =>
+                  navData.isActive ? classes.active : classes.link
+                }
+                to="/home/classics"
+              >
+                <li>Classics</li>
+              </NavLink>
+            </Navbar.Brand>
+
+            <Navbar.Brand>
+              <NavLink
+                className={(navData) =>
+                  navData.isActive ? classes.active : classes.link
+                }
+                to="/home/science-fiction"
+              >
+                <li>Science Fiction</li>
+              </NavLink>
+            </Navbar.Brand>
           </Nav>
           <button>Log In</button>
         </Container>
