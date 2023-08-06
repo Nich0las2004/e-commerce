6de028c3026fdcb51e6e46d4d14dae0cc764abbe
Nika Oniani <nikaoniani2018@gmail.com>
2023-08-07 00:00:16 +0400

    navbar button decoration finished

--- src/templates/NavBar/NavBarButton/NavBarButton.js
+++ src/templates/NavBar/NavBarButton/NavBarButton.js
@@ -2,10 +2,12 @@ import { BsCart } from "react-icons/bs";
 
 import classes from "./NavBarButton.module.css";
 
+import Badge from "react-bootstrap/Badge";
+
 const NavBarButton = () => {
   return (
     <button className={classes.button}>
-      <BsCart /> <span>0</span>
+      <BsCart /> <Badge className={classes.allBooks}>0</Badge>
     </button>
   );
 };
